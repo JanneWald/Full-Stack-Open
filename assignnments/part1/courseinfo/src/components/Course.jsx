ce74a24fa89127ab4d8c0563478698aafb2e6c34
jannewald <janne.l.wald@gmail.com>
2025-05-25 16:30:35 -0600

    2.3 exercises

--- assignnments/part1/courseinfo/src/components/Course.jsx
+++ assignnments/part1/courseinfo/src/components/Course.jsx
@@ -1,16 +1,24 @@
 const Parts = ({parts}) => {
     console.log(parts)
-    let sum = 0
-    parts.forEach(part => {
-        sum += part.exercises
-    });
+    console.log("reducing")
+
+    // Get total of exercises
+    const total = parts.reduce(
+        (sum, part) => {
+            console.log("Reducing part:", part)
+            return sum + part.exercises}, 0 // 0 is init value of sum
+    )
+    console.log("total:", total)
+
+    // Map each exercise to a new p
     return (
         <div>
             {parts.map(part => <p key={part.id}>{part.name}, {part.exercises}</p>) }
-            <b>Sum of exercises is {sum}</b>
+            <b>Sum of exercises is {total}</b>
         </div> 
     )
 }
+
 const Course = ({course}) => {
     const {parts, name, id} = course
     console.log(name)
