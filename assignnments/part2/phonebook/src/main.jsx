80020d67affed2ccdf7949eb77985562de11c685
jannewald <janne.l.wald@gmail.com>
2025-06-18 09:56:49 -0600

    note on double rendering

--- assignnments/part2/phonebook/src/main.jsx
+++ assignnments/part2/phonebook/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// Getting rid of safe mode stops double rendering, thus double logs, obviously not ideal for real deployment
 createRoot(document.getElementById('root')).render(
     <App />
 )
