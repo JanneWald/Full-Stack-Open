da0c27cbe148c80bdfeae637fd5accbb593f7441
jannewald <janne.l.wald@gmail.com>
2025-07-02 14:58:50 -0600

    added cron, linked front->back

--- assignnments/part3/phonebook/backend/index.js
+++ assignnments/part3/phonebook/backend/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
-const morgan = require('morgan')
-
-
-
+app.use(cors())
 app.use(express.json())
-app.use(morgan(function (tokens, req, res) {
+app.use(morgan(function (tokens, req, res) { // for example only, display json passing
   return [
     tokens.method(req, res),
     tokens.url(req, res),
@@ -129,7 +129,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 // Main
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
