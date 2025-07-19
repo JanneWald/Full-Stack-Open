32934b88e05a32016382cb7ab1eaaf706e757120
John Server <janne.l.wald@gmail.com>
2025-07-19 08:16:33 -0600

    working post/get

--- assignnments/part4/bloglist/backend/controllers/blogs.js
+++ assignnments/part4/bloglist/backend/controllers/blogs.js
@@ -23,8 +23,10 @@ blogsRouter.post('/', (request, response, next) => {
   const body = request.body
 
   const blog = new Blog({
-    content: body.content,
-    important: body.important || false,
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
   })
 
   blog.save()
--- assignnments/part4/bloglist/backend/models/blog.js
+++ assignnments/part4/bloglist/backend/models/blog.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose')
 
-const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
-  important: Boolean,
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
 })
 
-noteSchema.set('toJSON', {
+
+blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -17,4 +16,4 @@ noteSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model('Blog', blogSchema)
