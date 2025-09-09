2dff4b7ce639d4c604fc4957470164aecc38b305
John Server <janne.l.wald@gmail.com>
2025-09-09 14:27:53 -0600

    4.4

--- assignnments/part4/bloglist/backend/tests/dummy.test.js
+++ assignnments/part4/bloglist/backend/tests/dummy.test.js
@@ -4,10 +4,38 @@ Test cases for node --test
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const { setUncaughtExceptionCaptureCallback } = require('node:process')
+
+// Testing data
+// Note: only making json objects with likes since is the only thing relevant to our exercises
+const oneBlog = [{"likes":12}]
+const manyBlogs = []
+for(let i = 0; i < 25; i++){
+  manyBlogs.push({"likes":1})
+}
+const subtractingBlogs = [{"likes":1}, {"likes":-4}]
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+
+  test('of one blog', () => {
+      total = listHelper.totalLikes(oneBlog)
+      assert.strictEqual(total, 12)
+  })
+
+  test('of many blogs', () => {
+      total = listHelper.totalLikes(manyBlogs)
+      assert.strictEqual(total, 25)
+  })
+
+    test('of subtracting blogs', () => {
+      total = listHelper.totalLikes(subtractingBlogs)
+      assert.strictEqual(total, -3)
+  })
 })
--- assignnments/part4/bloglist/backend/utils/list_helper.js
+++ assignnments/part4/bloglist/backend/utils/list_helper.js
@@ -1,9 +1,20 @@
 /* List comprehension helper functions */
 
+// Returns 1 from any blog
 const dummy = (blogs) => {
   return 1
 }
 
+// Sums all the likes in a list of blogs
+const totalLikes = (blogs) => {
+    sum = 0
+    blogs.map(blog => {
+        sum += blog.likes
+    })
+    return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
