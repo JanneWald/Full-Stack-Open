85a381ec7fad79cf6f91390161fad94d4681df6e
John Server <janne.l.wald@gmail.com>
2025-09-10 11:31:26 -0600

    4.10

--- assignnments/part4/bloglist/backend/controllers/blogs.js
+++ assignnments/part4/bloglist/backend/controllers/blogs.js
@@ -24,7 +24,7 @@ blogsRouter.get('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
   const blog = new Blog({
@@ -34,11 +34,8 @@ blogsRouter.post('/', (request, response, next) => {
     likes: body.likes,
   })
 
-  blog.save()
-    .then(savedBlog => {
-      response.status(201).json(savedBlog)
-    })
-    .catch(error => next(error))
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', (request, response, next) => {
