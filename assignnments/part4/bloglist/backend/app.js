10127d7849e9a782c70b393182539636b9709d7a
John Server <janne.l.wald@gmail.com>
2025-09-12 18:01:52 -0600

    4.18

--- assignnments/part4/bloglist/backend/app.js
+++ assignnments/part4/bloglist/backend/app.js
@@ -10,6 +10,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -28,6 +29,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
