6d26ac26653cd17eb10bea305d6796d0fe556586
John Server <janne.l.wald@gmail.com>
2025-09-21 10:08:57 -0600

    5.7

--- assignnments/part5/bloglist/frontend/src/components/Blog.jsx
+++ assignnments/part5/bloglist/frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailedView, setDetailedView] = useState(false)
+  const {title, author, url, likes} = blog
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  if (detailedView) {
+    return (
+      <div style={blogStyle}>
+        <p><i>{title}</i> <button onClick={() => {setDetailedView(false)}}>Hide Details</button></p>
+        <p>By: {author}</p>
+        <p>{url}</p>
+        <p>Likes: {likes} <button>Like</button></p>
+      </div>
+    )
+  }
+  else{
+    return(  
+    <div style={blogStyle}>
+      <i>{title}</i>, {author} <button onClick={() => {setDetailedView(true)}}>Show Details</button>
+    </div>)
+  }
+}
 
 export default Blog
