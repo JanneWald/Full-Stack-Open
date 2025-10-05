a0a01042afca8ad30a06605eec2eda160a8b91ef
John Server <janne.l.wald@gmail.com>
2025-10-05 11:54:30 -0600

    5.18

--- assignnments/part4/bloglist/backend/app.js
+++ assignnments/part4/bloglist/backend/app.js
@@ -11,6 +11,7 @@ const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const testingRouter = require('./controllers/testing')
 
 const app = express()
 
@@ -34,6 +35,7 @@ app.use(middleware.requestLogger)
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/testing', testingRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- assignnments/part5/bloglist/playwright/tests/blog.spec.js
+++ assignnments/part5/bloglist/playwright/tests/blog.spec.js
@@ -1,8 +1,22 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
+const loginWith = async (page, username, password) => {
+  await page.getByLabel('username').fill(username)
+  await page.getByLabel('password').fill(password)
+  await page.getByRole('button', { name: 'login' }).click()
+} 
+
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
     await page.goto('/')
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'root user',
+        username: 'root',
+        password: '123456'
+      }
+    })
   })
 
   test('Login form is shown', async ({ page }) => {
@@ -10,4 +24,15 @@ describe('Blog app', () => {
     await expect(locator.first()).toBeVisible()
     await expect(locator.last()).toBeVisible()
   })
+
+  test('Succesful Login', async ({ page }) => {
+    await loginWith(page, 'root', '123456')
+    await expect(page.getByText('root logged in')).toBeVisible()
+   })
+
+  test('Unsuccesful Login', async ({ page }) => {
+    await loginWith(page, 'root', '1234567')
+    await expect(page.getByText('Wrong credentials')).toBeVisible()
+  })
+
 })
