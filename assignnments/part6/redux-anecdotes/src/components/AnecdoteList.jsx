c4ffcc33c3d322981a7c5c9c908b60c1ea8bb533
John Server <janne.l.wald@gmail.com>
2025-10-08 10:07:20 -0600

    6.14

--- assignnments/part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ assignnments/part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,9 +1,17 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { voteAnecdote, setAnecdotes } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
+import { useEffect } from 'react'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAll()
+      .then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+
 
   const filter = useSelector(state => state.filter)
   const anecdotes = useSelector(state => state.anecdotes)
