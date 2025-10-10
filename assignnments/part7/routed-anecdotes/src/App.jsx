aa771209adfc306572204f8ebeed44d63b3e187c
John Server <janne.l.wald@gmail.com>
2025-10-10 12:06:25 -0600

    7.1

--- assignnments/part7/routed-anecdotes/src/App.jsx
+++ assignnments/part7/routed-anecdotes/src/App.jsx
@@ -4,6 +4,8 @@ import About from './components/About'
 import CreateNew from './components/CreateNew'
 import Footer from './components/Footer'
 import Menu from './components/Menu'
+import Home from './components/Home'
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
@@ -45,14 +47,20 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <h1>Software anecdotes</h1>
+        <Menu />
+
+        <Routes>
+          <Route path='/' element={<Home anecdotes={anecdotes} />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/create' element={<CreateNew addNew={addNew}/>}/>
+        </Routes>
+
+        <Footer /> 
+      </div>
+    </Router>
   )
 }
 
---
+++ assignnments/part7/routed-anecdotes/src/components/Home.jsx
@@ -0,0 +1,7 @@
+import AnecdoteList from "./AnecdoteList"
+
+const Home = ({ anecdotes }) => (
+  <AnecdoteList anecdotes={anecdotes} />
+)
+
+export default Home
