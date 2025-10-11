82b525249b53f425b6830571018acaa5c843dee6
John Server <janne.l.wald@gmail.com>
2025-10-11 11:10:40 -0600

    7.8 but actually 7.7 and 7.8 are flipped because im stupid

--- assignnments/part7/ultimate-hooks/src/App.jsx
+++ assignnments/part7/ultimate-hooks/src/App.jsx
@@ -17,11 +17,17 @@ const useField = (type) => {
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
-
-  // ...
+  
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then(response => setResources(response.data))
+  }, [baseUrl])
 
   const create = (resource) => {
-    // ...
+    axios
+      .post(baseUrl, resource)
+      .then(response => setResources(resources.concat([response.data])))
   }
 
   const service = {
