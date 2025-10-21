9ee6fe53981088fa741d337017444e624c107874
John Server <janne.l.wald@gmail.com>
2025-10-21 10:43:04 -0600

    7.14

--- assignnments/part7/final-proj-bloglist/frontend/src/components/Users.jsx
+++ assignnments/part7/final-proj-bloglist/frontend/src/components/Users.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import userService from '../services/users';
+
 const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    userService.getAll().then((response) => setUsers(response.data)); // axios wraps data
+  }, []);
+
+  console.log(users); // now contains your users once fetched
+
   return (
     <>
       <h1>Users page</h1>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Count</th>
+        </tr>
+
+        {users.map((user) => (
+          <tr>
+            <td>{user.name}</td>
+            <td>{user.blogs.length}</td>
+          </tr>
+        ))}
+      </table>
     </>
   );
 };
